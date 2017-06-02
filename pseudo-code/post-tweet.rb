b51b7946289f6383fcc258baa90a1023054b62bf
@snowman <jimmoffitt@yahoo.com>
2017-06-02 17:36:25 -0600

    Create post-tweet.rb

---
+++ pseudo-code/post-tweet.rb
@@ -0,0 +1,35 @@
+```ruby
+require 'twitter'
+
+class PostTweet
+
+   attr_accessor :config,
+				 :consumer_key, :consumer_secret, :access_token, :access_token_secret,
+				 :client
+
+   def initialize(config_file)
+	  @config = getConfig(config_file)
+
+	  @client = Twitter::REST::Client.new do |config|
+		 config.consumer_key        = @config['consumer_key']
+		 config.consumer_secret     = @config['consumer_secret']
+		 config.access_token        = @config['access_token']
+		 config.access_token_secret = @config['access_token_secret']
+	  end
+
+   end
+
+   def post(message)
+  
+	  tweet = @client.update message
+
+	  #Including 4 photos:
+	  media_ids = %w(./images/nws_1.png ./images/nws_2.png ./images/nws_3.png ./images/nws_4.png).map do |filename|
+	    client.upload File.new filename
+	  end
+	  
+    message = "Tweeting with four photos."
+	  tweet = client.update message, {media_ids: media_ids.join(',')}
+   end
+end
+```
