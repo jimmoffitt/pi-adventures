5f0e1b73051d30afbb5f91cf3f66d79175430c07
@snowman <jimmoffitt@yahoo.com>
2017-06-07 14:33:24 -0600

    Update webhook_consumer.rb

--- pseudo-code/webhook_consumer.rb
+++ pseudo-code/webhook_consumer.rb
@@ -10,22 +10,19 @@ class EnrollerApp < Sinatra::Base
 
 	get '/' do
 		"Welcome to the @FloodSocial notification system! <br>
-          This websocket component is used for enrolling subscribers into a geo-aware, Twitter-based notification system. <br>
-          This component is a consumer of Account Activity API events, and uses Direct Message API to communicate to recipient account.
-     "
+                This websocket component is used for enrolling subscribers into a geo-aware, Twitter-based notification system. <br>
+                This component is a consumer of Account Activity API events, and uses Direct Message API to communicate to recipient account."
 	end
 
 	# Receives challenge response check (CRC).
 	get '/webhooks/twitter' do
 		crc_token = params['crc_token']
 		response['response_token'] = "sha256=#{generate_crc_response(settings.dm_api_consumer_secret, crc_token)}"
-    body response.to_json
-    status 200
-	end
+ 	end
 
 	# Receives DM events.
 	post '/webhooks/twitter' do
 		event = request.body.read
 		manager.handle_event(event)
-  end
+        end
 end
