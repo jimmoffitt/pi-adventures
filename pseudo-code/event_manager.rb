2f6cc67ac18ed24a96b71f5bb937e4d8961c9a40
@snowman <jimmoffitt@yahoo.com>
2017-06-12 14:00:50 -0600

    Create event_manager.rb

--- pseudo-code/event_manager.rb
+++ pseudo-code/event_manager.rb
@@ -1,3 +1,5 @@
+
+#Snippet #1
 #POST requests to /webhooks/twitter arrive here.
 #Twitter Account Activity API send events as POST requests with DM JSON payloads.
 
@@ -16,43 +18,35 @@ def handle_event(events)
     end
 end
 
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-				if dm_event['type'] == 'message_create'
 
-					#Is this a response? Test for the 'quick_reply_response' key.
-					is_response = dm_event['message_create'] && dm_event['message_create']['message_data'] && dm_event['message_create']['message_data']['quick_reply_response']
+#Snippet #2
+#Seeing if we have a DM payload to process...	  	  
+if dm_event['type'] == 'message_create'
 
-					if is_response
-						response = dm_event['message_create']['message_data']['quick_reply_response']['metadata']
-						user_id = dm_event['message_create']['sender_id']
+	#Is this a response? Test for the 'quick_reply_response' key.
+	is_response = dm_event['message_create'] && dm_event['message_create']['message_data'] && dm_event['message_create']['message_data']['quick_reply_response']
 
-						if response == 'add_area'
-							@DMSender.send_area_method(user_id)
-							#And many other response 'types'
-						end
-					else
-						#Since this DM is not a response to a QR, let's check for other 'action' commands
-						#puts 'Received a command/question DM? Need to track conversation stage?'
+	if is_response #then grab 'metadata' to see which DM user is responding to... 
+		response = dm_event['message_create']['message_data']['quick_reply_response']['metadata']
+		user_id = dm_event['message_create']['sender_id']
 
-						request = dm_event['message_create']['message_data']['text']
-						user_id = dm_event['message_create']['sender_id']
+		if response == 'add_area' 
+			@DMSender.send_area_method(user_id)
+			#And many other response 'types'
+		end
+	else
+		#Since this DM is not a response to a QR, let's check for other 'action' commands
+		#puts 'Received a command/question DM? Need to track conversation stage?'
 
-						if request.length < COMMAND_MESSAGE_LIMIT and (request.downcase.include? 'home' or request.downcase.include? 'add' or request.downcase.include? 'main' or request.downcase.include? 'hello')
-							puts 'Send QR to add an area'
-							#Send QR for which 'select area' method
-							@DMSender.send_welcome_message(user_id)
-						
-						end
-					end
-				end
-			end
+		request = dm_event['message_create']['message_data']['text']
+		user_id = dm_event['message_create']['sender_id']
+
+		if request.length < COMMAND_MESSAGE_LIMIT and (request.downcase.include? 'home' or request.downcase.include? 'add' or request.downcase.include? 'main' or request.downcase.include? 'hello')
+			puts 'Send QR to add an area'
+			#Send QR for which 'select area' method
+			@DMSender.send_welcome_message(user_id)
+
 		end
 	end
 end
+			
